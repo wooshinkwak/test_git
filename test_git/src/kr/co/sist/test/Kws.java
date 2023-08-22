ff438be6129123f2f334e45e142bf6da39e96ee2
user <user@DESKTOP-COJ9858>
2023-08-22 12:40:54 +0900

    머지2

--- test_git/src/kr/co/sist/test/Kws.java
+++ test_git/src/kr/co/sist/test/Kws.java
@@ -4,6 +4,8 @@ public class Kws {
 
 	public static void main(String[] args) {
 		System.out.println("이거슨 신기능 추가!!!!");
+		System.out.println("대포동 미사일");
+		System.out.println("임태균의 미소!! ^_^");
 	}
 
 }
